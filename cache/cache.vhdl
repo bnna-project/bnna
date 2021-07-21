1b99dfe822890a4c78787d9098927caead0bcec8
Thorben Simon <thorsimon.simon@gmail.com>
2021-07-21 15:52:16 +0200

    updated cache & processing_unit

--- cache/cache.vhdl
+++ cache/cache.vhdl
@@ -9,31 +9,40 @@ entity cache is
     port(
         reset: in std_logic;
         clk: in std_logic;
-        input1: in std_logic_vector(2 downto 0);
-        output1: out std_logic_vector(31 downto 0);
+        input1: in std_logic_vector(3 downto 0);
+        output1: out std_logic_vector(7 downto 0);
         ready: out std_logic
     );
 end cache;
 
 architecture bhv of cache is
-    signal cache_register: std_logic_vector(31 downto 0);
+    signal cache_register: std_logic_vector(7 downto 0);
 begin
     process(clk)
-    variable temp: unsigned(31 downto 0) := b"00000000000000000000000000000000";
+    variable tin: unsigned(7 downto 0) := b"00000000";
+    variable tcache: unsigned(7 downto 0) := b"00000000";
     begin
         if rising_edge(clk) then
             ready <= '0';
-
             if(reset = '1') then
-                cache_register <= b"00000000000000000000000000000000";
+                cache_register <= b"00000000";
             else
-                temp := resize(unsigned(input1), cache_register'length);
-                temp := temp + unsigned(cache_register);
+                tin := resize(unsigned(input1(2 downto 0)), cache_register'length);
+                tcache := unsigned(cache_register);
+                if(input1(3) = '1') then
+                    tin := tin or b"11111000";
+                end if;
+                tin := tin + tcache;
+                cache_register <= std_logic_vector(tin);
             end if;
+            ready <= '1';
+        end if;
 
-            cache_register <= std_logic_vector(temp);
-            output1 <= cache_register;
-
+        if falling_edge(clk) then
+            ready <= '0';
+            if(reset = '0') then
+                output1 <= cache_register;
+            end if;
             ready <= '1';
         end if;
 
--- processing_unit/processing_unit.vhdl
+++ processing_unit/processing_unit.vhdl
@@ -29,7 +29,7 @@ entity processing_unit is
         component pop_count
         port(
             input1: in std_logic_vector(NUMBER_OF_W_A -1 downto 0);
-            output1: out std_logic_vector(NUMBER_OF_BITs_out -1 downto 0)
+            output1: out std_logic_vector(NUMBER_OF_BITs_out downto 0)
         );
         end component;
 
@@ -37,8 +37,8 @@ entity processing_unit is
         port(
             reset: in std_logic;
             clk: in std_logic;
-            input1: in std_logic_vector(2 downto 0);
-            output1: out std_logic_vector(31 downto 0);
+            input1: in std_logic_vector(NUMBER_OF_BITs_out downto 0);
+            output1: out std_logic_vector(7 downto 0);
             ready: out std_logic
         );
         end component;
@@ -52,58 +52,14 @@ entity processing_unit is
         end component;
 
         signal popcount_in: std_logic_vector(NUMBER_OF_W_A -1 downto 0);
-        signal popcount_out: std_logic_vector(NUMBER_OF_BITs_out -1 downto 0);
-        signal cache_out: std_logic_vector(31 downto 0);
+        signal popcount_out: std_logic_vector(NUMBER_OF_BITs_out downto 0);
+        signal cache_out: std_logic_vector(7 downto 0);
 
         begin
             --8 xnor gates connected to one popcount with 8 inputs and 3 outputs
-            xnor0: xnor_2 port map(
-                data => data(0),
-                weight => weight(0),
-                output1 => popcount_in(0)
-            );
-
-            xnor1: xnor_2 port map(
-                data => data(1),
-                weight => weight(1),
-                output1 => popcount_in(1)
-            );
-
-            xnor2: xnor_2 port map(
-                data => data(2),
-                weight => weight(2),
-                output1 => popcount_in(2)
-            );
-
-            xnor3: xnor_2 port map(
-                data => data(3),
-                weight => weight(3),
-                output1 => popcount_in(3)
-            );
-
-            xnor4: xnor_2 port map(
-                data => data(4),
-                weight => weight(4),
-                output1 => popcount_in(4)
-            );
-
-            xnor5: xnor_2 port map(
-                data => data(5),
-                weight => weight(5),
-                output1 => popcount_in(5)
-            );
-
-            xnor6: xnor_2 port map(
-                data => data(6),
-                weight => weight(6),
-                output1 => popcount_in(6)
-            );
-
-            xnor7: xnor_2 port map(
-                data => data(7),
-                weight => weight(7),
-                output1 => popcount_in(7)
-            );
+            gen: for i in 0 to (NUMBER_OF_W_A -1) generate
+            xnori: xnor_2 port map(data=>data(i), weight=>weight(i), output1=>popcount_in(i));
+            end generate;
 
             pop_count1: pop_count port map(
                 input1 => popcount_in,
@@ -118,7 +74,7 @@ entity processing_unit is
             );
 
             comparator1: comparator port map(
-                a => cache_out(7 downto 0),
+                a => cache_out,
                 b => treshold,
                 a_comp_b => output1
             );
